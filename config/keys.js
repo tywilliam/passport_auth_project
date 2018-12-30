b3f71147875d60511855d6451dc5bb3cc2e2881c
Tyrese Williams <tyresewilliams@Tyreses-MacBook-Pro.local>
2018-12-29 21:24:30 -0500

    initial work-

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://root:<PASSWORD>@cluster0-jyr4o.mongodb.net/test?retryWrites=true'
+}
---
+++ routes/users.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+// Login Page
+router.get('/login', (req, res) => res.render('login'));
+
+
+// Register Page
+router.get('/register', (req, res) => res.render('register'));
+
+// Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 }  = req.body;
+    let errors = [];
+
+    // Check required fields
+    if(!name || !email || !password || !password2) {
+        errors.push({ msg: ' Please fill in all fields'});
+    }
+    // Check if passwords match
+    if(password !== password2) {
+        errors.push({msg: 'Passwords do not match'});
+    }
+    // Check pass length
+    if(password.length < 6) {
+        errors.push({msg: 'Password should be at least 6 character'});
+    }
+    if(errors.length > 0) {
+        res.render('register', {
+            errors
+        });
+    } else {
+        res.send('pass')
+    }
+});
+module.exports = router;
